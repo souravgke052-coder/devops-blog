92f41f5cfcb7a1559353f28a11684f1b84693b05
sourav lobra <souravlobra@MacBook-Pro.local>
2026-06-13 22:40:22 +0530

    Deploy website - based on 566820c5bde8995ecc10f45add8272645c3f863d

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/souravlobra/devops-blog/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/souravlobra/devops-blog/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/souravlobra/devops-blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/souravlobra/devops-blog/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/souravlobra/devops-blog/src/css/custom.css"),
-];
